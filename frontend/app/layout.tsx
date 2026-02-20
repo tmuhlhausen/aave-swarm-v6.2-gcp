9d4b125e68e5e50c26d3433192755a2518dd1a92
tmuhlhausen <58753672+tmuhlhausen@users.noreply.github.com>
2026-02-19 23:44:07 -0500

    Create layout.tsx

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,9 @@
+import './globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="dark">
+      <body className="min-h-screen overflow-hidden">{children}</body>
+    </html>
+  );
+}
