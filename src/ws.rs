e986ed9bffd70093e38b13e7c4605f8ba2de7351
tmuhlhausen <58753672+tmuhlhausen@users.noreply.github.com>
2026-02-21 01:16:04 -0500

    Create ws.rs

---
+++ src/ws.rs
@@ -0,0 +1,26 @@
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use std::sync::Arc;
+use tokio::sync::broadcast;
+
+pub async fn ws_handler(ws: WebSocketUpgrade, tx: Arc<broadcast::Sender<String>>) -> impl IntoResponse {
+    ws.on_upgrade(|socket| handle_socket(socket, tx))
+}
+
+async fn handle_socket(mut socket: WebSocket, tx: Arc<broadcast::Sender<String>>) {
+    let mut rx = tx.subscribe();
+    loop {
+        tokio::select! {
+            msg = rx.recv() => {
+                if let Ok(msg) = msg {
+                    let _ = socket.send(Message::Text(msg)).await;
+                }
+            }
+            _ = socket.recv() => break, // client disconnected
+        }
+    }
+}
